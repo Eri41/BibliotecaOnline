e018264293b791cfbf3099ab61ebe1a7481e38c8
Eri41 <eriomar_kaka@hotmail.com>
2026-06-07 00:15:41 -0300

    API Online

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = 3000;
@@ -6,5 +7,5 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-/////////////////////////////////////////////////////////////
+
 
